07a5ff0a608cdcb5b90534956f823530d2a6fbf5
Britton W <britton.wolfe1999@gmail.com>
2020-05-12 14:28:20 -0400

    added more enemy behavior

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ const offsety = 100;
 
 let bodies = [];
 
+let xpframes = 0;
+let xmframes = 0;
+let ypframes = 0;
+let ymframes = 0;
+const buffer = 10;
+const grid = 2;
+const maxes = 2;
+const espeed = 0.025;
+let score = 0;
+
 function drawBoard() {
     context.fillStyle = "blue";
     context.fillRect(offsetx, offsety, 300, 300)
@@ -21,41 +31,60 @@ let player = {
         context.fillRect((this.x * 100) + 5 + offsetx, (this.y * 100) + 5 + offsety, 90, 90);
     }
 }
+let goal = {
+    x: 0,
+    y: 0,
+    draw: function() {
+        context.fillStyle = "green";
+        context.fillRect((this.x * 100) + 30 + offsetx, (this.y * 100) + 30 + offsety, 40, 40);
+    },
+    resetPos: function() {
+        this.x = Math.round(Math.random() * 2);
+        this.y = Math.round(Math.random() * 2);
+        if(this.x === player.x && this.y === player.y)
+            this.resetPos();
+    }
+}
 
-/*basically not a*/class Enemy {
+class Enemy {
     constructor() {
-        this.dirx = Math.round(Math.random());
-        this.diry = Math.round(Math.random());
-        if(dirx === 0)
-            this.x = -1;
-        else
-            this.x = 3;
-        if(diry === 0)
-            this.y = -1;
-        else
-            this.y = 3;
+        this.axis = Math.round(Math.random());
+        this.dir = (Math.round(Math.random()) === 0) ? 1 : -1;
+        this.x = 0;
+        this.y = 0;
+        if(this.axis === 0) {
+            if(this.dir === 1)
+                this.x = -1;
+            else
+                this.x = 3;
+            this.y = Math.round(Math.random() * 2);
+        } else {
+            if(this.dir === 1)
+                this.y = -1;
+            else
+                this.y = 3;
+            this.x = Math.round(Math.random() * 2);
+        }
         bodies.push(this);
     }
     draw() {
         context.fillStyle = "red";
-        context.fillRect((this.x * 100) + 15 + offsetx, (this.y * 100) + 15 + offsety, 70, 70);
+        context.fillRect(Math.round(this.x * 100) + 20 + offsetx, Math.round(this.y * 100) + 20 + offsety, 70, 70);
     }
+    move() {
+        if(this.axis === 0)
+            this.x += espeed * this.dir;
+        else
+            this.y += espeed * this.dir;
+        if(this.x > 3 || this.y > 3 || this.x < -1 || this.y < -1)
+            bodies.splice(bodies.indexOf(this), 1);
+        }
 }
 
 setInterval(loop, 20);
 
-let xpframes = 0;
-let xmframes = 0;
-let ypframes = 0;
-let ymframes = 0;
-const buffer = 10;
-const grid = 2;
-const maxes = 3;
-
 function loop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    if(Math.random() < 0.05 && bodies.length <= maxes)
-        new Enemy();
     if(Keyboarder.isDown(37)) {
         if(xmframes++ === 0 || xmframes > buffer)
             player.x--;
@@ -86,6 +115,10 @@ function loop() {
         player.y = grid;
     drawBoard();
     player.draw();
-    for(enemy of bodies)
+    for(enemy of bodies) {
+        enemy.move();
         enemy.draw();
+    }
+    if(Math.random() < 0.05 && bodies.length <= maxes)
+        new Enemy();
 }
